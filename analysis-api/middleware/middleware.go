fb0ef471a3cc2b3eb602fcd155943117acabda08
Boris Lachev <boris.lachevbg00@gmail.com>
2021-02-23 00:06:43 +0200

    Add token authentication middleware - disabled for now

--- analysis-api/middleware/middleware.go
+++ analysis-api/middleware/middleware.go
@@ -1,13 +1,20 @@
 package middleware
 
 import (
-"github.com/grpc-ecosystem/go-grpc-middleware"
-"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
-"github.com/grpc-ecosystem/go-grpc-middleware/tags"
-"go.uber.org/zap"
-"go.uber.org/zap/zapcore"
-"google.golang.org/grpc"
-"google.golang.org/grpc/codes"
+	"context"
+
+	"github.com/vectorman1/analysis/analysis-api/model"
+
+	"github.com/dgrijalva/jwt-go"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 // codeToLevel redirects OK to DEBUG level logging instead of INFO
@@ -20,26 +27,49 @@ func codeToLevel(code codes.Code) zapcore.Level {
 	return grpc_zap.DefaultCodeToLevel(code)
 }
 
-// AddLogging returns grpc.Server config option that turn on logging.
-func AddLogging(logger *zap.Logger, opts []grpc.ServerOption) []grpc.ServerOption {
+// LoadMiddleware returns grpc.Server config option with loaded middlewares
+func LoadMiddleware(logger *zap.Logger, opts []grpc.ServerOption) []grpc.ServerOption {
 	// Shared options for the logger-grpc, with a custom gRPC code to log level function.
 	o := []grpc_zap.Option{
 		grpc_zap.WithLevels(codeToLevel),
 	}
+
 	// Make sure that log statements internal to gRPC library are logged using the zapLogger as well.
 	grpc_zap.ReplaceGrpcLoggerV2(logger)
 
+	// Add authentication
 	// Add unary interceptor
 	opts = append(opts, grpc_middleware.WithUnaryServerChain(
+		//grpc_auth.UnaryServerInterceptor(authorizeToken),
 		grpc_ctxtags.UnaryServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
 		grpc_zap.UnaryServerInterceptor(logger, o...),
 	))
 
-	// Add stream interceptor (added as an example here)
+	// Add stream interceptor
 	opts = append(opts, grpc_middleware.WithStreamServerChain(
 		grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
 		grpc_zap.StreamServerInterceptor(logger, o...),
 	))
 
 	return opts
+}
+
+// authorizeToken parses the token and stores the user info in the request context
+func authorizeToken(ctx context.Context) (context.Context, error) {
+	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
+	if err != nil {
+		return nil, err
+	}
+
+	claims := &model.Token{}
+	_, err = jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	newCtx := context.WithValue(ctx, "user_info", claims)
+
+	return newCtx, nil
 }

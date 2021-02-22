c9b263e6238cf9fef1cde58fb8d5f6a4adc5f294
Boris Lachev <boris.lachevbg00@gmail.com>
2021-02-23 00:11:41 +0200

    update worker entry file

--- analysis-worker/worker.go
+++ analysis-worker/worker.go
@@ -6,21 +6,30 @@ import (
 	"github.com/vectorman1/analysis/analysis-worker/generated/trading212_service"
 	"github.com/vectorman1/analysis/analysis-worker/service"
 	"google.golang.org/grpc"
+	"log"
 	"net"
 )
 
 
 
 func main() {
-	alaskalog.Logger.Infoln("Starting server...")
+	cfg, err := common.GetConfig()
+	if err != nil {
+		log.Fatalf("failed to get config: %v", err)
+	}
+
+	log.Println("Starting server...")
 	lis, err := net.Listen("tcp", ":6969")
 	if err != nil {
-		alaskalog.Logger.Fatalf("Failed to listen on port :6969: %v", err)
+		log.Fatalf("Failed to listen on port :6969: %v", err)
 	}
+	log.Println("listening on port :6969")
+
 	tls, err := common.LoadTLSCredentials()
 	if err != nil {
-		alaskalog.Logger.Fatalf("failed to read TLS config: %v", err)
+		log.Fatalf("failed to read TLS config: %v", err)
 	}
+	log.Println("loaded TLS config")
 
 	s := grpc.NewServer(
 		grpc.Creds(tls))
@@ -28,7 +37,10 @@ func main() {
 		service.Trading212Service{}.New(
 			common.TRADING212_INSTRUMENTS_LINK,
 			common.TRADING212_SHOW_ALL_BUTTON_SELECTOR,
-			common.TRADING212_ALL_INSTRUMENTS_SELECTOR))
+			common.TRADING212_ALL_INSTRUMENTS_SELECTOR,
+			cfg))
+
+	log.Println("initialized grpc server")
 
 	if err := s.Serve(lis); err != nil {
 		alaskalog.Logger.Fatalf("server failed with: %v", err)
